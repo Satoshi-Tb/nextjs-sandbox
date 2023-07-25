ec8b19bf09c1c2d16556996ff88a01c8cec1512f
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2023-07-26 01:33:55 +0900

    別パタンの編集UI追加中

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 import parse from "html-react-parser";
 
+/** ラジオボタン設定 */
+interface Radio {
+  label: string;
+  value: string;
+}
+
 const sampleText = `
 React (リアクト) は、Meta（旧Facebook）とコミュニティによって開発されているユーザインタフェース構築のためのJavaScriptライブラリである。React.jsまたはReactJSの名称でも知られている。
 Reactはシングルページアプリケーションやモバイルアプリケーションの開発におけるベースとして使用することができる。複雑なReactアプリケーションでは通常、状態管理・ルーティング・APIとの対話のための追加のライブラリが必要となる。
-Next.js（ネクストジェイエス）は、Node.js上に構築されたオープンソースの<span style='text-decoration: solid underline purple 4px;'>Webアプリケーションフレームワーク</span>であり、サーバーサイドスクリプトや静的Webサイトの生成などの、ReactベースのWebアプリケーション機能を有効にする。
-  `;
+Next.js（ネクストジェイエス）は、Node.js上に構築されたオープンソースの<span style='text-decoration: solid underline purple 4px;'>Webアプリケーションフレームワーク</span>であり、
+サーバーサイドスクリプトや静的Webサイトの生成などの、ReactベースのWebアプリケーション機能を有効にする。
+`;
 
 function Home() {
   const highlightColors = ["yellow", "lime", "red"];
   const highlightColorNames = ["黄色", "緑色", "赤色"];
+  const [editMode, setEditMode] = useState(false);
+  const [enableSelect, setEnableSelect] = useState(false);
 
   const [selectedRange, setSelectedRange] = useState<Range | null>(null);
 
@@ -20,9 +29,23 @@ function Home() {
     applyHighlight(highlighColor);
   };
 
-  const handleOnDeleteClick = () => {
+  const handleOnClickDelete = () => {
     removeAllHighlights();
   };
+
+  const handleOnChangeEditMode = () => {
+    setEditMode((prev) => {
+      //set関数のネストはOKか？
+      setEnableSelect(!prev);
+      return !prev;
+    });
+  };
+
+  const [selectedHightlightColor, setSelectedHighlightColor] =
+    useState("yellow");
+
+  const changeValue = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setSelectedHighlightColor(event.target.value);
 
   const handleMouseUp = () => {
     const selection = window.getSelection();
@@ -33,10 +56,16 @@ function Home() {
     if (selection.rangeCount > 0) {
       setSelectedRange(selection.getRangeAt(0));
     }
+    if (editMode) {
+      //TODO うまくいかない
+      console.log(selectedHightlightColor);
+      applyHighlight(selectedHightlightColor);
+    }
   };
 
   //TODO このやり方はReactとして適切ではない可能性あり
   const applyHighlight = (color: string) => {
+    console.log(selectedRange);
     if (selectedRange) {
       const rangeContents = selectedRange.extractContents();
       Array.from(rangeContents.querySelectorAll(".highlight")).forEach(
@@ -94,23 +123,58 @@ function Home() {
           justifyContent: "space-around",
         }}
       >
-        <p>■マーカー色</p>
-        {highlightColors.map((c, i) => (
+        <div>
+          <p>■マーカー色</p>
+          {highlightColors.map((c, i) => (
+            <button
+              style={{ margin: "5px", paddingRight: "5px", paddingLeft: "5px" }}
+              onClick={handleOnClick}
+              key={i}
+              name={c}
+            >
+              {highlightColorNames[i]}
+            </button>
+          ))}
           <button
             style={{ margin: "5px", paddingRight: "5px", paddingLeft: "5px" }}
-            onClick={handleOnClick}
-            key={i}
-            name={c}
+            onClick={handleOnClickDelete}
           >
-            {highlightColorNames[i]}
+            クリア
           </button>
-        ))}
-        <button
-          style={{ margin: "5px", paddingRight: "5px", paddingLeft: "5px" }}
-          onClick={handleOnDeleteClick}
-        >
-          クリア
-        </button>
+        </div>
+        <hr></hr>
+        <div style={{ display: "flex" }}>
+          <input
+            type="checkbox"
+            checked={editMode}
+            onChange={handleOnChangeEditMode}
+            id="checkEditMode"
+            name="checkEditMode"
+            style={{ marginLeft: "5px" }}
+          />
+          <label htmlFor="checkEditMode">ライン編集</label>
+          {highlightColors.map((color, i) => {
+            return (
+              <div>
+                {/* checked属性に式を定義する */}
+                <input
+                  type="radio"
+                  name={`highlightColor-${color}`}
+                  value={color}
+                  checked={color === selectedHightlightColor}
+                  onChange={changeValue}
+                  key={color}
+                  id={`highlightColor-${color}`}
+                  style={{ marginLeft: "5px" }}
+                  disabled={!enableSelect}
+                />
+                <label htmlFor={`highlightColor-${color}`}>
+                  {highlightColorNames[i]}
+                </label>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
