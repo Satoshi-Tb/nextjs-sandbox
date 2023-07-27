6e3e4f69d67af0ff0bef9ee314244be982f471f4
Satoshi-Tb <sulp91_fy@yahoo.co.jp>
2023-07-28 00:46:56 +0900

    Lexicalエディター追加

---
+++ src/pages/samples/rich-editor/Editor.tsx
@@ -0,0 +1,47 @@
+import { ComponentProps, FC } from "react";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
+  namespace: "MyEditor",
+  onError: (error) => console.error(error),
+};
+
+export const Editor: FC = () => {
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <div
+        style={{
+          position: "relative",
+          padding: "24px",
+          minHeight: "240px",
+          background: "white",
+          border: "1px solid black",
+          borderRadius: "10px",
+          margin: "5px",
+        }}
+      >
+        <RichTextPlugin
+          contentEditable={<ContentEditable style={{ outline: "none" }} />}
+          placeholder={
+            <div
+              style={{
+                position: "absolute",
+                color: "#888888",
+                top: "24px",
+                left: "24px",
+                pointerEvents: "none",
+                userSelect: "none",
+              }}
+            >
+              いまなにしてる？
+            </div>
+          }
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+      </div>
+    </LexicalComposer>
+  );
+};
