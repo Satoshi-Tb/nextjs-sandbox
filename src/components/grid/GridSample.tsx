fc1568d6780b4922e148f2a4d6defedda65eed12
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2023-10-20 02:01:02 +0900

    fix:ページ切替対応

--- src/components/grid/GridSample.tsx
+++ src/components/grid/GridSample.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
 const rawData = [
@@ -9,23 +9,43 @@ const rawData = [
   { id: 5, category: "野菜", item: "なす" },
   { id: 6, category: "野菜", item: "きゅうり" },
   { id: 7, category: "野菜", item: "大根" },
+  { id: 8, category: "野菜", item: "ごぼう" },
+  { id: 9, category: "野菜", item: "キャベツ" },
+  { id: 10, category: "野菜", item: "長ネギ" },
+  { id: 11, category: "野菜", item: "玉ねぎ" },
+  { id: 12, category: "野菜", item: "にんじん" },
 ];
 
-let lastCategory = "";
-const processedData = rawData.map((data) => {
-  const isDuplicate = data.category === lastCategory;
-  lastCategory = data.category;
-  return {
-    ...data,
-    textColor: isDuplicate ? "white" : "black",
-  };
-});
+const INITIAL_PAGE_SIZE = 5;
 
 export const GridSample = () => {
+  const [pageSize, setPageSize] = useState(INITIAL_PAGE_SIZE);
+
+  const processedData = rawData.map((data, index, arr) => {
+    const isDuplicate =
+      index !== 0 && data.category === arr[index - 1].category;
+    const isFirstItemOfPage = index % pageSize === 0;
+    let textColor;
+
+    if (isFirstItemOfPage) {
+      textColor = "black";
+    } else {
+      textColor = isDuplicate ? "white" : "black";
+    }
+
+    return {
+      ...data,
+      textColor,
+    };
+  });
+
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
         rows={processedData}
+        initialState={{
+          pagination: { paginationModel: { pageSize: INITIAL_PAGE_SIZE } },
+        }}
         columns={[
           {
             field: "category",
@@ -42,6 +62,10 @@ export const GridSample = () => {
           },
         ]}
         pageSizeOptions={[5, 10, 25]}
+        onPaginationModelChange={(model) => {
+          console.log("paginationmodel", model);
+          setPageSize(model.pageSize);
+        }}
       />
     </div>
   );
