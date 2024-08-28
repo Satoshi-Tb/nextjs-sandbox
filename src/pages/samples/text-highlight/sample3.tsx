e039ec603ac54ee6b37c6d9eaad9fe995c6b4135
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2024-08-29 08:39:23 +0900

    WIP:半角全角サンプル実装中

--- src/pages/samples/text-highlight/sample3.tsx
+++ src/pages/samples/text-highlight/sample3.tsx
@@ -9,17 +9,31 @@ const highlightSettings = [
   { text: "version", color: "lightblue" },
 ];
 
+// 半角文字列に変換する関数
+const toHalfWidth = (str: string) =>
+  str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (ch) =>
+    String.fromCharCode(ch.charCodeAt(0) - 0xfee0)
+  );
+
 // 指定された文字列に基づいてHTMLテキストをハイライトする関数
 const highlightText = (text: string) => {
   // 各設定に対して正規表現を作成し、テキストを置換
   let highlightedText = text;
   highlightSettings.forEach(({ text: searchText, color }) => {
-    const regex = new RegExp(`(${searchText})`, "gi");
-    highlightedText = highlightedText.replace(
-      regex,
-      (match) =>
-        `<span style="background-color: ${color}; font-weight: bold;">${match}</span>`
-    );
+    const normalizedSearchText = toHalfWidth(searchText).toLowerCase(); // キーワードの正規化
+    const normalizedInputText = toHalfWidth(highlightedText).toLowerCase(); // 対象文字列の正規化
+
+    const regex = new RegExp(`(${normalizedSearchText})`, "gi");
+
+    highlightedText = normalizedInputText.replace(regex, (match) => {
+      // 元の文字列のマッチ部分を取得
+      // これだと不完全。マッチ部分の正確なインデックスを取得する必要がある。
+      const originalMatch = highlightedText.slice(
+        normalizedInputText.indexOf(match),
+        normalizedInputText.indexOf(match) + match.length
+      );
+      return `<span style="background-color: ${color}; font-weight: bold;">${originalMatch}</span>`;
+    });
   });
   return highlightedText;
 };
@@ -30,7 +44,7 @@ const HighLightSample3 = () => {
       <h1>Hello, World!</h1>
       <p>
       This is a sample HTML string with some text to <u>highlight</u>. Let's highlight the words 'highlight' and 'ハイライト' and 'version'.<br/>
-      Ignore upper/lower case: Version, VERSION
+      Ignore upper/lower case: Version, VERSION, ｖｅｒｓｉｏｎ
       <img src="https://www.j-platpat.inpit.go.jp/gazette_work/domestic/A/419289000/419289100/419289140/419289141/7239EB7A6A04F265EADF0B7910FBA631E4E0BFE2EACC7203C2F97822A65C5B3A/text/JPA 419289141_i_000004.jpg?version=202408280639"></img>
       </p>
     </div>
