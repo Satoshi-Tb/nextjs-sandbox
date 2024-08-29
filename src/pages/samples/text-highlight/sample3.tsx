e3f5980d24c6294a8e9d8f839734038e63c8aa92
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2024-08-30 08:20:20 +0900

    WIP:大文字小文字、全角半角対応

--- src/pages/samples/text-highlight/sample3.tsx
+++ src/pages/samples/text-highlight/sample3.tsx
@@ -10,6 +10,7 @@ const highlightSettings = [
 ];
 
 // 半角文字列に変換する関数
+// TODO 全角カナ対応
 const toHalfWidth = (str: string) =>
   str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (ch) =>
     String.fromCharCode(ch.charCodeAt(0) - 0xfee0)
@@ -17,23 +18,57 @@ const toHalfWidth = (str: string) =>
 
 // 指定された文字列に基づいてHTMLテキストをハイライトする関数
 const highlightText = (text: string) => {
-  // 各設定に対して正規表現を作成し、テキストを置換
   let highlightedText = text;
+
+  // ハイライト設定分、テキストを置換
+  // TODO 複数適用すると、2回目のループでハイライト用の<span>タグが置換対象になってしまう。この単純なやり方で実施する場合、ハイライトタグ名は検索対象外の文字列にしないとダメ。
   highlightSettings.forEach(({ text: searchText, color }) => {
-    const normalizedSearchText = toHalfWidth(searchText).toLowerCase(); // キーワードの正規化
-    const normalizedInputText = toHalfWidth(highlightedText).toLowerCase(); // 対象文字列の正規化
+    const normalizedSearchText = toHalfWidth(searchText).toLowerCase(); // キーワードのノーマライズ
+    const normalizedInputText = toHalfWidth(highlightedText).toLowerCase(); // 対象文字列のノーマライズ
 
-    const regex = new RegExp(`(${normalizedSearchText})`, "gi");
+    const regex = new RegExp(`(${normalizedSearchText})`, "g"); // 正規表現構築
 
-    highlightedText = normalizedInputText.replace(regex, (match) => {
-      // 元の文字列のマッチ部分を取得
-      // これだと不完全。マッチ部分の正確なインデックスを取得する必要がある。
-      const originalMatch = highlightedText.slice(
-        normalizedInputText.indexOf(match),
-        normalizedInputText.indexOf(match) + match.length
+    let match;
+    let resultStr = "";
+    let ptr = 0;
+    const matchResult: { index: number; len: number }[] = []; // 検索結果(検索ヒット位置と、キーワード文字数)
+    console.log("target", highlightedText);
+    console.log("search key", normalizedSearchText);
+
+    // ノーマライズ結果に対して、全ての検索ヒット位置を取得
+    while ((match = regex.exec(normalizedInputText)) !== null) {
+      console.log(
+        `Found ${match[0]} start=${match.index} end=${regex.lastIndex}.`
       );
-      return `<span style="background-color: ${color}; font-weight: bold;">${originalMatch}</span>`;
+      matchResult.push({
+        index: match.index,
+        len: normalizedSearchText.length,
+      });
+    }
+
+    matchResult.forEach((m, i) => {
+      resultStr += highlightedText.slice(ptr, m.index);
+      resultStr +=
+        `<span style="background-color: ${color}; font-weight: bold;">` +
+        highlightedText.slice(m.index, m.index + m.len) +
+        "</span>";
+      ptr = m.index + m.len;
     });
+
+    resultStr += highlightedText.slice(ptr);
+    console.log("match result", matchResult);
+    console.log("highlight result", resultStr);
+    highlightedText = resultStr;
+    // highlightedText = normalizedInputText.replace(regex, (match, args) => {
+    //   console.log("regex.lastIndex", regex.lastIndex);
+    //   // 元の文字列のマッチ部分を取得
+    //   // これだと不完全。マッチ部分の正確なインデックスを取得する必要がある。
+    //   const originalMatch = highlightedText.slice(
+    //     normalizedInputText.indexOf(match),
+    //     normalizedInputText.indexOf(match) + match.length
+    //   );
+    //   return `<span style="background-color: ${color}; font-weight: bold;">${originalMatch}</span>`;
+    // });
   });
   return highlightedText;
 };
@@ -43,8 +78,7 @@ const HighLightSample3 = () => {
     <div>
       <h1>Hello, World!</h1>
       <p>
-      This is a sample HTML string with some text to <u>highlight</u>. Let's highlight the words 'highlight' and 'ハイライト' and 'version'.<br/>
-      Ignore upper/lower case: Version, VERSION, ｖｅｒｓｉｏｎ
+      This is a sample HTML string with some text to <u>highlight</u>. Let's highlight the words 'highlight' and 'ハイライト' and 'version'.<br/>Ignore upper/lower case: Version, VERSION, ｖｅｒｓｉｏｎ, ＶerＳION
       <img src="https://www.j-platpat.inpit.go.jp/gazette_work/domestic/A/419289000/419289100/419289140/419289141/7239EB7A6A04F265EADF0B7910FBA631E4E0BFE2EACC7203C2F97822A65C5B3A/text/JPA 419289141_i_000004.jpg?version=202408280639"></img>
       </p>
     </div>
