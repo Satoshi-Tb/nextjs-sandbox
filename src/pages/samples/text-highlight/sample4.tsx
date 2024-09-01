722b98d61c69dabb8ce39b55381f9e4de729e053
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2024-09-01 15:13:01 +0900

    簡易表記ゆれ対応（大文字小文字、全角半角、カタカナひらがな）

--- src/pages/samples/text-highlight/sample4.tsx
+++ src/pages/samples/text-highlight/sample4.tsx
@@ -17,7 +17,7 @@ type HighlightSetting = {
 // オプションサンプル：タグ除去
 const optTagStrip: HTMLReactParserOptions = {
   replace: (domNode: DOMNode) => {
-    console.dir(domNode, { depth: null });
+    // console.dir(domNode, { depth: null });
     // DOMNodeがElement（タグ）である場合、除去(<></>に置換)
     if (domNode instanceof Element && domNode.type === "tag") {
       // 再帰的に子要素を処理する
@@ -39,16 +39,22 @@ const highlightText = (text: string, keyword: string, color: string) => {
 };
 
 // ハイライト設定
-const highlightSetting: HighlightSetting = { keyword: "GPT", color: "yellow" };
+const highlightSetting: HighlightSetting = {
+  keyword: "GPT",
+  color: "yellow",
+};
 const highlightSettings = [highlightSetting, { keyword: "open", color: "red" }];
 
 // ハイライトサンプル1
 const optHighlight1: HTMLReactParserOptions = {
   replace: (domNode: DOMNode) => {
-    console.dir(domNode, { depth: null });
+    // console.dir(domNode, { depth: null });
     // Textノードを処理
     if (domNode instanceof Text) {
       const text = domNode.data;
+
+      // キーワードで元のtextを分割し、キーワードをハイライトタグでラップしたJSXオブジェクトを追加しながら
+      // JSXまたはstringの配列を構築する
       const jsxElements = text
         .split(highlightSetting.keyword)
         .reduce((prev, curr, i) => {
@@ -62,6 +68,203 @@ const optHighlight1: HTMLReactParserOptions = {
           prev.push(curr);
           return prev;
         }, [] as (string | JSX.Element)[]);
+
+      return <>{jsxElements}</>;
+    }
+    return;
+  },
+};
+
+// 英数字全角→半角英数
+const toHalfWidth = (str: string) =>
+  str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (ch) =>
+    String.fromCharCode(ch.charCodeAt(0) - 0xfee0)
+  );
+
+// 半角英数→全角英数
+const toFullWidth = (str: string) =>
+  str.replace(/[A-Za-z0-9]/g, function (s) {
+    return String.fromCharCode(s.charCodeAt(0) + 0xfee0);
+  });
+
+// カタカナ→ひらがな
+const kanaToHira = (str: string) =>
+  str.replace(/[\u30a1-\u30f6]/g, function (s) {
+    return String.fromCharCode(s.charCodeAt(0) - 0x60);
+  });
+
+// ひらがな→カタカナ
+const hiraToKana = (str: string) =>
+  str.replace(/[\u3041-\u3096]/g, function (s) {
+    return String.fromCharCode(s.charCodeAt(0) + 0x60);
+  });
+
+// 半角カナ→全角カナ
+// キーワード検索処理の都合、濁音、半濁音記号は独立した１文字として変換する（文字数が変わらないようにしたいため）
+const kanaHalfToFull = (str: string) => {
+  const kanaMap: { [key: string]: string } = {
+    // ｶﾞ: "カ゛",
+    // ｷﾞ: "キ゛",
+    // ｸﾞ: "ク゛",
+    // ｹﾞ: "ケ゛",
+    // ｺﾞ: "コ゛",
+    // ｻﾞ: "サ゛",
+    // ｼﾞ: "シ゛",
+    // ｽﾞ: "ス゛",
+    // ｾﾞ: "セ゛",
+    // ｿﾞ: "ソ゛",
+    // ﾀﾞ: "タ゛",
+    // ﾁﾞ: "チ゛",
+    // ﾂﾞ: "ツ゛",
+    // ﾃﾞ: "テ゛",
+    // ﾄﾞ: "ト゛",
+    // ﾊﾞ: "ハ゛",
+    // ﾋﾞ: "ヒ゛",
+    // ﾌﾞ: "フ゛",
+    // ﾍﾞ: "ヘ゛",
+    // ﾎﾞ: "ホ゛",
+    // ﾊﾟ: "ハ゜",
+    // ﾋﾟ: "ヒ゜",
+    // ﾌﾟ: "フ゜",
+    // ﾍﾟ: "ヘ゜",
+    // ﾎﾟ: "ホ゜",
+    // ｳﾞ: "ウ゛",
+    // ﾜﾞ: "ワ゛",
+    // ｦﾞ: "ヲ゛",
+    ｱ: "ア",
+    ｲ: "イ",
+    ｳ: "ウ",
+    ｴ: "エ",
+    ｵ: "オ",
+    ｶ: "カ",
+    ｷ: "キ",
+    ｸ: "ク",
+    ｹ: "ケ",
+    ｺ: "コ",
+    ｻ: "サ",
+    ｼ: "シ",
+    ｽ: "ス",
+    ｾ: "セ",
+    ｿ: "ソ",
+    ﾀ: "タ",
+    ﾁ: "チ",
+    ﾂ: "ツ",
+    ﾃ: "テ",
+    ﾄ: "ト",
+    ﾅ: "ナ",
+    ﾆ: "ニ",
+    ﾇ: "ヌ",
+    ﾈ: "ネ",
+    ﾉ: "ノ",
+    ﾊ: "ハ",
+    ﾋ: "ヒ",
+    ﾌ: "フ",
+    ﾍ: "ヘ",
+    ﾎ: "ホ",
+    ﾏ: "マ",
+    ﾐ: "ミ",
+    ﾑ: "ム",
+    ﾒ: "メ",
+    ﾓ: "モ",
+    ﾔ: "ヤ",
+    ﾕ: "ユ",
+    ﾖ: "ヨ",
+    ﾗ: "ラ",
+    ﾘ: "リ",
+    ﾙ: "ル",
+    ﾚ: "レ",
+    ﾛ: "ロ",
+    ﾜ: "ワ",
+    ｦ: "ヲ",
+    ﾝ: "ン",
+    ｧ: "ァ",
+    ｨ: "ィ",
+    ｩ: "ゥ",
+    ｪ: "ェ",
+    ｫ: "ォ",
+    ｯ: "ッ",
+    ｬ: "ャ",
+    ｭ: "ュ",
+    ｮ: "ョ",
+    "｡": "。",
+    "､": "、",
+    ｰ: "ー",
+    "｢": "「",
+    "｣": "」",
+    "･": "・",
+    ﾞ: "゛",
+    ﾟ: "゜",
+  };
+
+  const reg = new RegExp("(" + Object.keys(kanaMap).join("|") + ")", "g");
+  return str.replace(reg, (match) => kanaMap[match] || match);
+};
+
+// ・キーワードハイライト処理用の文字列ノーマライズ関数
+//   英数字：大文字→小文字＆全角→半角
+//   上記以外（カタカナ、ひらがな、記号）：半角→全角＆カタカナ→ひらがな
+//   * 濁音、半濁音のマッチは不可能　ﾊﾞ⇔バ、ﾊﾟ⇔パ等
+//   * 拗音、吃音の揺らぎ対応はしない　ぁ⇔あ、っ⇔つ等
+// ・ライブラリ利用について
+// jaconv 5.3KB
+// moji 4.1KB
+// snyk adviserでチェックしたが、どちらも差異はないか？
+// 気になるのはメンテ名が少ない事（1, 2名）。直近メンテが数年前であること
+// https://snyk.io/advisor/
+// →ライブラリ容量や、メンテナンス、濁音・半濁音の対応を考えると自作でも良いように思える
+// →jaconvはJava版がある。フロントとバックで仕様統一できる。
+// →パフォーマンスで参考になることがあれば
+const normalizeText = (src: string) => {
+  let result = src;
+  result = toHalfWidth(result);
+  result = result.toLowerCase();
+  result = kanaHalfToFull(result);
+  result = kanaToHira(result);
+  return result;
+};
+
+// ハイライトサンプル2
+const optHighlight2: HTMLReactParserOptions = {
+  replace: (domNode: DOMNode) => {
+    // console.dir(domNode, { depth: null });
+    // Textノードを処理
+    if (domNode instanceof Text) {
+      const textOrg = domNode.data;
+
+      // キーワード検索のため、キーワードと検索対象文字列をノーマライズ
+      const normalizedSearchText = normalizeText(highlightSetting.keyword);
+      const normalizedInputText = normalizeText(textOrg);
+
+      // キーワードヒット位置を取得
+      const regex = new RegExp(`(${normalizedSearchText})`, "g"); // 正規表現構築
+
+      let match;
+      const matchResult: { index: number; len: number }[] = []; // 検索結果(検索ヒット位置と、キーワード文字数)
+
+      // ノーマライズ結果に対して、全ての検索ヒット位置を取得
+      while ((match = regex.exec(normalizedInputText)) !== null) {
+        // console.log(
+        //   `Found ${match[0]} start=${match.index} end=${regex.lastIndex}.`
+        // );
+        matchResult.push({
+          index: match.index,
+          len: normalizedSearchText.length,
+        });
+      }
+
+      // 元の文書から検索ヒット位置に該当するワードを抽出し、ハイライトタグを付与
+      const jsxElements: (string | JSX.Element)[] = [];
+      let ptr = 0;
+      matchResult.forEach((m) => {
+        jsxElements.push(textOrg.slice(ptr, m.index));
+        jsxElements.push(
+          <span style={{ backgroundColor: highlightSetting.color }}>
+            {textOrg.slice(m.index, m.index + m.len)}
+          </span>
+        );
+        ptr = m.index + m.len;
+      });
+      jsxElements.push(textOrg.slice(ptr));
 
       return <>{jsxElements}</>;
     }
@@ -93,10 +296,18 @@ const HighLightSample4 = () => {
       <hr />
       <DisplayCard
         key={3}
-        title="ハイライト"
+        title="ハイライトその１"
         description="単一ワードのみ/完全一致（大文字小文字や、全角半角対応なし）"
         planeHtml={htmlString}
         parseOptions={optHighlight1}
+      />
+      <hr />
+      <DisplayCard
+        key={4}
+        title="ハイライトその２"
+        description="単一ワードのみ/大文字小文字、全角半角区別なし"
+        planeHtml={htmlString}
+        parseOptions={optHighlight2}
       />
       <div style={{ display: "flex" }}>
         <div style={{ marginTop: "20px" }}>
